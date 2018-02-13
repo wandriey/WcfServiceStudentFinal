52e39ee83931dab356802fce7e35e1c9fd503f2f
wandriey <jona434g@edu.easj.dk>
2018-02-13 14:26:52 +0100

    .

---
+++ WcfServiceStudentFinalTests/Service1Tests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfServiceStudentFinal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceStudentFinal.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void Service1Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddStudentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindStudentsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllStudentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveStudentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditStudentTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
